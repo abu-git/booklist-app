1e567718cb41e73a9c839873397e61bc9ec5e343
abu-git <abu.sammie@gmail.com>
2020-08-17 16:43:43 +0200

    added reducer and localStorage

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -5,28 +5,23 @@
 	if item matches a CERTAIN CONDITION e.g book.id !== id (it returned true) 
 	it keeps it in array else (it returned false) it removes it
 */
-import React, { createContext, useState } from 'react';
-import { v4 } from 'uuid';
+import React, { createContext, useReducer, useEffect } from 'react';
+import { bookReducer } from '../reducers/bookReducer';
 
 //note: same as name of file
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-	const [books, setBooks] = useState([
-		{title: 'name of the wind', author: 'patrick rothfuss', id: 1},
-		{title: 'the final empire', author: 'brandon sanderson', id: 2,}
-	]);
+	const [books, dispatch] = useReducer(bookReducer, [], () =>{
+		const localData = localStorage.getItem('books');
+		return localData ? JSON.parse(localData) : [];
+	});
 
-	const addBook = (title, author) => {
-		setBooks([...books, { title, author, id: v4() }]);
-	};
-
-	const removeBook = (id) => {
-		setBooks(books.filter(book => book.id !== id));
-	};
-
+	useEffect(() =>{
+		localStorage.setItem('books', JSON.stringify(books))
+	}, [books]);
 	return(
-		<BookContext.Provider value={{ books, addBook, removeBook }}>
+		<BookContext.Provider value={{ books, dispatch }}>
 			{props.children}
 		</BookContext.Provider>
 	);
